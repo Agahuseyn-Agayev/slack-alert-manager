e83c7da704b3d8e4f77e018ef6f078bd4f5769c4
Your Name <you@example.com>
2025-02-10 17:26:07 +0400

    initial commit for slack-alert-manager project

---
+++ src/main/java/com/example/slackalertmanager/SlackAlertManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.slackalertmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackAlertManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackAlertManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slackalertmanager/SlackAlertManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slackalertmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackAlertManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
